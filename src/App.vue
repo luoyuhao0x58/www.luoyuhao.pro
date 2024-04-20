7ed495b7ecf09053b1e4d006664f1b7e522db1dc
Luo Yuhao <luoyuhao@haujilo.xyz>
2024-04-20 13:43:55 +0800

    feat: 实现首页。

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Profile from './components/Profile.vue'
+</script>
+
+<template>
+  <div>
+      <picture>
+        <source srcset="./assets/avatar.avif" type="image/avif" />
+        <source srcset="./assets/avatar.webp" type="image/webp" />
+        <img src="./assets/avatar.png" class="logo" alt="avatar" />
+      </picture>
+  </div>
+  <Profile msg="Luo Yuhao" />
+</template>
+
+<style scoped>
+.logo {
+  height: 12em;
+  will-change: filter;
+  transition: filter 300ms;
+  filter: drop-shadow(0 0 2px #FFF);
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #ECCED0);
+}
+</style>
---
+++ src/components/Profile.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+  <h1 class="title">{{ msg }}</h1>
+
+</template>
+
+<style scoped>
+.title {
+  margin-top: 2px;
+  font-size: 3.2em;
+  line-height: 1.1;
+}
+</style>
